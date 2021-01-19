0b7e1761828cc1d4b2aee5a34373b488222e07b6
CMRoss <ChrisRoss@u.boisestate.edu>
2021-01-18 20:24:36 -0700

    Unit Testing example

---
+++ TestNinja.UnitTests/ReservationTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting; //needed for using unit testing framework
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    //CTRL R and then R again to rename Class
+    [TestClass]
+    public class ReservationTest //convention is NameOfClassTest
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue() //convention is NameOfMethod_Scenario_ExpectedBehavior
+        {
+            //Arrange - where we initialize our objects
+            var reservation = new Reservation();
+
+            //Act - where we act on this object (call method we are going to test) (store as a result)
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true}); //give the method a user that is an admin user
+
+            //Assert -  where we verify that the result is correct by passing it the result
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_MadeByUser_ReturnsTrue()
+        {
+            //Arrange
+            var user = new User();                              //simulated user
+            var reservation = new Reservation{MadeBy = user};   //set MadeBy to our simulated user
+
+            //Act
+            var result = reservation.CanBeCancelledBy(user);    //canceled by simulated user
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCancellingRes_ReturnsFalse()
+        {
+            //Arrange
+            var reservation = new Reservation {MadeBy = new User()};   //set MadeBy to a simulated user
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User());    //canceled by a different simulated user
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
--- TestNinja/Fundamentals/Reservation.cs
+++ TestNinja/Fundamentals/Reservation.cs
@@ -6,7 +6,22 @@
 
         public bool CanBeCancelledBy(User user)
         {
-            return (user.IsAdmin || MadeBy == user);
+            //simple buisness rules
+            //three scenarios (execution paths) - when user, when admin, when someone else
+            //if (user.IsAdmin)
+            //    return true;
+
+            //if (MadeBy == user)
+            //    return true;
+
+            //simple buisness rules (refactor 2)
+            //if (user.IsAdmin || MadeBy == user) //rewrite code and test again to make sure everything still works
+            //    return true;
+
+            //return false;
+
+            //simple buisness rules (refactor 3)
+            return (user.IsAdmin || MadeBy == user); //rewrite code and test again to make sure everything still works
         }
         
     }
